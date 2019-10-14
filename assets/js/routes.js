9350576daf88dd02dac8fa9ecae1447ae3c07f98
Bernard Bos <bernardbos@gmail.com>
2019-10-14 20:54:55 +0200

    Fix routing issues introduced by yours truly

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -3,11 +3,26 @@ import NewAssessment from './components/PageComponents/NewAssessment.vue';
 import Browse from './components/PageComponents/Browse.vue';
 import Settings from './components/PageComponents/Settings.vue';
 
-const routes = [
-    { path: '/', component: Dashboard, alias: '/dashboard' },
-    { path: '/new-assessment', component: NewAssessment },
-    { path: '/browse', component: Browse },
-    { path: '/settings', component: Settings },
+const routes = [{
+		path: '/',
+		component: Dashboard
+	},
+	{
+		path: '/dashboard',
+		component: Dashboard
+	},
+	{
+		path: '/new-assessment',
+		component: NewAssessment
+	},
+	{
+		path: '/browse',
+		component: Browse
+	},
+	{
+		path: '/settings',
+		component: Settings
+	}
 ];
 
 export default routes;
