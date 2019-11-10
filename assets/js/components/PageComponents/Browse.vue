54b5272f438627e85a6a7a90c17e192ae611d873
Joost Reijmer <s1120128@student.windesheim.nl>
2019-11-10 16:17:16 +0100

    $attrs$listeners

--- assets/js/components/PageComponents/Browse.vue
+++ assets/js/components/PageComponents/Browse.vue
@@ -6,98 +6,62 @@
           <i class="fa fa-arrow-left"></i> Terug naar dashboard
         </router-link>
       </div>
-      <div class="col-6">
-        <h2>Bladeren</h2>
-      </div>
+      
     </div>
 
-    <!--De title die ik aan b-tab hieronder meegeef wordt niet gerenderd in html, als ik dezelfde code in codepen/fiddle probeer lukt het wel. Weet jij waarom?-->
-    <!--Er verschijnen 10 vue-errors bij het laden van deze pagina:-->
-    <!--5 x [Vue warn]: $attrs is readonly-->
-    <!--5 x [Vue warn]: $listeners is readonly-->
-    <!--Deze verschijnen paarsgewijs bij 1 x b-tabs, 3 x b-tab en 1 x b-pagination: weet jij hoe dit te voorkomen is?-->
-    <!--Er verschijnt nog een 11e vue-error bij totalRows: weet jij hoe ik dit kan oplossen?-->
-    <!--Er verschijnt nog een 12e vue-error als je van pagina wisselt: value (wat verwijst naar currentPageAll). Weet jij hoe ik dit zou kunnen oplossen?-->
-    <div>
-      <b-card no-body>
-        <b-tabs pills card>
-          <b-tab title="Tab 1" active><b-card-text>Tab Contents 1</b-card-text></b-tab>
-          <b-tab title="Tab 2"><b-card-text>Tab Contents 2</b-card-text></b-tab>
-          <b-tab title="Tab 3"><b-card-text>Tab Contents 3</b-card-text></b-tab>
-        </b-tabs>
-      </b-card>
-    </div>
+    <tabs>
+      <tab title="Alle beoordelingen">
+        </tab>
+      <tab title="Mijn beoordelingen">
+        </tab>
+    </tabs>
+    
+    <b-pagination v-model="currentPage"
+                  :total-rows="rows"
+                  :per-page="perPage"
+                  aria-controls="assessment-listAll"></b-pagination>
 
-    <div v-if="navigationToggle">
-      <b-pagination v-model="currentPageAll"
-                    :total-rows="rowsAll"
-                    :per-page="perPage"
-                    aria-controls="assessment-listAll"></b-pagination>
+    <p class="mt-3">Current Page: {{ currentPageAll }}</p>    
 
-      <p class="mt-3">Current Page: {{ currentPageAll }}</p>
-
-      <div id="assessment-listAll">
-        <div class="card" v-for="item in itemsAll" v-bind:key="item.id">
-          <div class="card-body">
-            <h5 class="card-title">
-              Beoordeling {{ item.vak }}
-              <span v-for="student in item.student"
-                    v-bind:key="student.account">{{ student.naam }}({{ student.account }})</span>
-            </h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{ item.code }}</h6>
-            <a href="#" class="btn btn-info">Open beoordeling</a>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">
-              Laatst bijgewerkt: {{ item.date_last_modified }} door
-              <span v-for="examinator in item.examinator"
-                    v-bind:key="examinator.account">{{ examinator.naam }}({{ examinator.account }})</span>
-            </small>
-          </div>
+    <div id="assessment-listAll">
+      <div class="card" v-for="item in itemsAll" v-bind:key="item.id">
+        <div class="card-body">
+          <h5 class="card-title">
+            Beoordeling {{ item.vak }}
+            <span v-for="student in item.student"
+                  v-bind:key="student.account">{{ student.naam }}({{ student.account }})</span>
+          </h5>
+          <h6 class="card-subtitle mb-2 text-muted">{{ item.code }}</h6>
+          <a href="#" class="btn btn-info">Open beoordeling</a>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">
+            Laatst bijgewerkt: {{ item.date_last_modified }} door
+            <span v-for="examinator in item.examinator"
+                  v-bind:key="examinator.account">{{ examinator.naam }}({{ examinator.account }})</span>
+          </small>
         </div>
       </div>
+
     </div>
-
-    <div v-if="!navigationToggle">
-      <b-pagination v-model="currentPageAccount"
-                    :total-rows="rowsAccount"
-                    :per-page="perPage"
-                    aria-controls="assessment-listAccount"></b-pagination>
-
-      <p class="mt-3">Current Page: {{ currentPageAccount }}</p>
-
-      <div id="assessment-listAccount">
-        <div class="card" v-for="item in itemsAccount" v-bind:key="item.id">
-          <div class="card-body">
-            <h5 class="card-title">
-              Beoordeling {{ item.vak }}
-              <span v-for="student in item.student"
-                    v-bind:key="student.account">{{ student.naam }}({{ student.account }})</span>
-            </h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{ item.code }}</h6>
-            <a href="#" class="btn btn-info">Open beoordeling</a>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">
-              Laatst bijgewerkt: {{ item.date_last_modified }} door
-              <span v-for="examinator in item.examinator"
-                    v-bind:key="examinator.account">{{ examinator.naam }}({{ examinator.account }})</span>
-            </small>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    </div>    
 </template>
 
 <script>
+  import Vue from 'vue'
   import axios from "axios";
-  import { BTabs, BTab, BCard, BCardText, BPagination } from 'bootstrap-vue'
+  import { BPagination, BTable } from 'bootstrap-vue'
+  import { Tabs, Tab } from 'vue-slim-tabs'
+
+  Vue.component('b-pagination', BPagination)
 
   export default {
     name: "browse",
     data() {
       return {
+        perPage: 3,
+        currentPage: 1,
+
         // algemeen
         navigationToggle: true,
         perPage: 1,
@@ -105,15 +69,177 @@
         // voor lijst met alle beoordelingen
         currentPageAll: 1,
         rowsAll: null,
-        itemsAll: [],  
+        itemsAll: [{
+          id: 1,
+          status: "Draft",
+          vak: "Afstuderen",
+          code: "ICT.AFSTSE.V19",
+          student: [{ account: "S1019744", naam: "Bernard Bos" }],
+          date_created: "09-10-2019 22:12",
+          date_last_modified: "09-10-2019 22:16",
+          examinator: [{ account: "BV0111997", naam: "Eugen van Roden" }]
+        },
+        {
+          id: 2,
+          status: "Draft",
+          vak: "Stage",
+          code: "ICT.STAGE.V19",
+          student: [{ account: "S1120990", naam: "Karen Bosch" }],
+          date_created: "06-10-2019 08:13",
+          date_last_modified: "06-10-2019 08:18",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 3,
+          status: "Draft",
+          vak: "Afstuderen",
+          code: "ICT.AFSTSE.V19",
+          student: [{ account: "S1019744", naam: "Bernard Bos" }],
+          date_created: "09-10-2019 22:12",
+          date_last_modified: "09-10-2019 22:16",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 4,
+          status: "Draft",
+          vak: "Stage",
+          code: "ICT.STAGE.V19",
+          student: [{ account: "S1120990", naam: "Karen Bosch" }],
+          date_created: "06-10-2019 08:13",
+          date_last_modified: "06-10-2019 08:18",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 5,
+          status: "Draft",
+          vak: "Afstuderen",
+          code: "ICT.AFSTSE.V19",
+          student: [{ account: "S1019744", naam: "Bernard Bos" }],
+          date_created: "09-10-2019 22:12",
+          date_last_modified: "09-10-2019 22:16",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 6,
+          status: "Draft",
+          vak: "Stage",
+          code: "ICT.STAGE.V19",
+          student: [{ account: "S1120990", naam: "Karen Bosch" }],
+          date_created: "06-10-2019 08:13",
+          date_last_modified: "06-10-2019 08:18",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 7,
+          status: "Draft",
+          vak: "Afstuderen",
+          code: "ICT.AFSTSE.V19",
+          student: [{ account: "S1019744", naam: "Bernard Bos" }],
+          date_created: "09-10-2019 22:12",
+          date_last_modified: "09-10-2019 22:16",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 8,
+          status: "Draft",
+          vak: "Stage",
+          code: "ICT.STAGE.V19",
+          student: [{ account: "S1120990", naam: "Karen Bosch" }],
+          date_created: "06-10-2019 08:13",
+          date_last_modified: "06-10-2019 08:18",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 9,
+          status: "Draft",
+          vak: "Afstuderen",
+          code: "ICT.AFSTSE.V19",
+          student: [{ account: "S1019744", naam: "Bernard Bos" }],
+          date_created: "09-10-2019 22:12",
+          date_last_modified: "09-10-2019 22:16",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 10,
+          status: "Draft",
+          vak: "Stage",
+          code: "ICT.STAGE.V19",
+          student: [{ account: "S1120990", naam: "Karen Bosch" }],
+          date_created: "06-10-2019 08:13",
+          date_last_modified: "06-10-2019 08:18",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 11,
+          status: "Draft",
+          vak: "Afstuderen",
+          code: "ICT.AFSTSE.V19",
+          student: [{ account: "S1019744", naam: "Bernard Bos" }],
+          date_created: "09-10-2019 22:12",
+          date_last_modified: "09-10-2019 22:16",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 12,
+          status: "Draft",
+          vak: "Stage",
+          code: "ICT.STAGE.V19",
+          student: [{ account: "S1120990", naam: "Karen Bosch" }],
+          date_created: "06-10-2019 08:13",
+          date_last_modified: "06-10-2019 08:18",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 13,
+          status: "Draft",
+          vak: "Afstuderen",
+          code: "ICT.AFSTSE.V19",
+          student: [{ account: "S1019744", naam: "Bernard Bos" }],
+          date_created: "09-10-2019 22:12",
+          date_last_modified: "09-10-2019 22:16",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 14,
+          status: "Draft",
+          vak: "Stage",
+          code: "ICT.STAGE.V19",
+          student: [{ account: "S1120990", naam: "Karen Bosch" }],
+          date_created: "06-10-2019 08:13",
+          date_last_modified: "06-10-2019 08:18",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 15,
+          status: "Draft",
+          vak: "Afstuderen",
+          code: "ICT.AFSTSE.V19",
+          student: [{ account: "S1019744", naam: "Bernard Bos" }],
+          date_created: "09-10-2019 22:12",
+          date_last_modified: "09-10-2019 22:16",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        },
+        {
+          id: 16,
+          status: "Draft",
+          vak: "Stage",
+          code: "ICT.STAGE.V19",
+          student: [{ account: "S1120990", naam: "Karen Bosch" }],
+          date_created: "06-10-2019 08:13",
+          date_last_modified: "06-10-2019 08:18",
+          examinator: [{ account: "BV0111996", naam: "Arjen Korevaar" }]
+        }],
 
-        // voor lijst met beoordelingen van beoordelaar met ingelogde account
+         //voor lijst met beoordelingen van beoordelaar met ingelogde account
         currentPageAccount: 1,
-        rowsAccount: null,
+        rowsAccount: 6,
         itemsAccount: [],
       };
     },
-    methods: {      
+    methods: {
+      onComplete: function () {
+        alert('Yay. Done!');
+      },
       getAssessments: function () {
         const ENDPOINTS = "Assessment/GetAssessments/";
         axios
@@ -155,14 +281,89 @@
             }
             this.rowsAll = this.itemsAll.length;
           });
+        this.rowsAll = this.itemsAll.length;
       }
     },
     components: {
-      'b-tabs': BTabs,
-      'b-tab': BTab,
-      'b-card': BCard,
-      'b-card-text': BCardText,
-      'b-pagination': BPagination
+      
+      Tabs,
+      Tab,
+      'b-table': BTable
+    },
+    computed: {
+      rows() {
+        return this.itemsAll.length
+      },
+      currentpage: {
+        get: function () {
+          alert("get currentpage");
+          if (this.navigationToggle) {
+            return this.currentPageAll
+          } else {
+            return this.currentPageAccount
+          }
+        },
+        set: function (newValue) {
+          alert("set currentpage")
+          if (this.navigationToggle) {
+            this.currentPageAll = newValue
+          } else {
+            this.currentPageAccount = newValue
+          }
+        }
+      },
+      assessmentlist: function () {
+        alert("assessmentlist");
+        if (this.navigationToggle) {
+          return this.itemsAll
+        } else {
+          return this.itemsAccount
+        }
+      },
+      perpage: function () {
+        alert("perpage")
+        return this.perPage
+      },
+      rowsx: function () {
+        if (this.navigationToggle) {
+          return this.rowsAll
+        } else {
+          return this.rowsAccount
+        }
+      },
+      currentpagex: {
+        get: function () {
+          if (this.navigationToggle) {
+            return this.currentPageAll
+          } else {
+            return this.currentPageAccount
+          }
+        },
+        set: function (newValue) {
+          if (this.navigationToggle) {
+            this.currentPageAll = newValue
+          } else {
+            this.currentPageAccount = newValue
+          }
+        }
+      },
+      assessmentlistx: function () {
+        alert("assessmentlistx")
+        if (this.navigationToggle) {
+          return this.itemsAll
+        } else {
+          return this.itemsAccount
+        }
+      },
+      perpagex: function () {
+        return this.perPage
+      },
+      currentTabComponent: function () {
+        return 'b-pagination'
+      },
+      nextTabComponent: function () {
+        return 'b-pagination'
+      }
     },
     created: function () {
       this.getAssessments();
