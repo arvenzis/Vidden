d659bf77495812a05f84be9616626277aaf6660b
Bernard Bos <bb0057329@windesheim.nl>
2019-11-13 22:03:03 +0100

    Route to edit with the id of the examinator

--- assets/js/components/PageComponents/Summary.vue
+++ assets/js/components/PageComponents/Summary.vue
@@ -11,10 +11,9 @@
             <section id="assessment-list">
                 <div v-for="item in response" v-bind:key="item.metadataId">
                     <div class="card" v-for="assessment in item.assessments" v-bind:key="assessment.id">
-                        <div class="card-body">
-                            <h5 class="card-title mb-3">Invulling door <span v-for="examinator in assessment.examinator" v-bind:key="examinator.account">{{ examinator.name }}
-                                ({{ examinator.account }})</span></h5>
-                            <router-link :to="'/edit/' + assessment.id">
+                        <div class="card-body" v-for="examinator in assessment.examinator" v-bind:key="examinator.account">
+                            <h5 class="card-title mb-3">Invulling door {{ examinator.name }} ({{ examinator.account }})</h5>
+                            <router-link :to="'/edit/' + item.metadataId + '/' + examinator.id ">
                                 <button class="btn btn-info">Open invulling</button>
                             </router-link>
                         </div>
@@ -42,13 +41,16 @@
                 // For now mock data
                 response: [{
                     metadataId: this.$route.params.id,
+                    templateId: 1,
                     status: 1,
                     oeCode: "ICTAFSTSE.D19",
                     student: [{
+                        id: 4,
                         account: "S1019744",
                         name: "Bernard Bos"
                     }],
                     examinator: [{
+                        id: 6,
                         account: "BV0111996",
                         name: "Arjen Korevaar"
                     }],
@@ -60,6 +62,7 @@
                             date_created: "12-11-2019 11:47",
                             date_last_modified: "13-11-2019 09:18",
                             examinator: [{
+                                id: 6,
                                 account: "BV0111996",
                                 name: "Arjen Korevaar"
                             }]
@@ -69,39 +72,51 @@
                             date_created: "12-11-2019 11:47",
                             date_last_modified: "13-11-2019 09:18",
                             examinator: [{
-                                account: "P98273498",
+                                id: 7,
+                                account: "P69750694",
                                 name: "Eugene van Roden"
                             }]
                         },
                     ]
                 }],
-                loading: false
+                loading: false,
+                template: ""
             }
         },
-        // created() {
-        //     this.loading = true;
-        //     const ENDPOINTS = 'Assessment/' + this.id;
-        //     axios.get(this.$store.state.apiBaseUrl + ENDPOINTS, {
-        //             headers: {
-        //                 "Authorization": this.$session.get('jwt')
-        //             }
-        //         })
-        //         .then(response => {
-        //             this.loading = false;
-        //             // Do something
-        //         })
-        //         .catch(error => {
-        //              .this.loading = false;
-        //              // Do something
-        //         });
-        // },
+        created() {
+            // this.loading = false;
+            // const ENDPOINTS = 'Assessment/' + this.id;
+            // axios.get(this.$store.state.apiBaseUrl + ENDPOINTS, {
+            //         headers: {
+            //             "Authorization": this.$session.get('jwt')
+            //         }
+            //     })
+            //     .then(response => {
+            //         this.loading = false;
+            //         // Do something
+            //     })
+            //     .catch(error => {
+            //         .this.loading = false;
+            //         // Do something
+            //     });
+        },
         methods: {
             fullNameStudent(array) {
                 return array[0].student[0].name + ' (' + array[0].student[0].account + ')';
             },
             fullNameTeacher(array) {
                 return array[0].examinator[0].name + ' (' + array[0].examinator[0].account + ')';
-            }    
+            },
+            getTemplate(id) {
+                const ENDPOINTS = 'Template/' + id;
+                axios.get(this.$store.state.apiBaseUrl + ENDPOINTS, 
+                    { 
+                        headers: { "Authorization" : this.$session.get('jwt') } 
+                    })
+                    .then(response => {
+                        this.template = response.name;
+                    });
+            }   
         },
         components: {
             Spinner
