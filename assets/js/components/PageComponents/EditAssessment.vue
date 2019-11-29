c593bcc2b8689c63038ab80eae1955c4856ca92c
Karen Brakband <karen@impres.nl>
2019-11-29 09:52:04 +0100

    Remove comment

--- assets/js/components/PageComponents/EditAssessment.vue
+++ assets/js/components/PageComponents/EditAssessment.vue
@@ -265,7 +265,6 @@
                                 "answer":comment.answer
                             }
                         ]
-                        // "userId": this.examinatorId,
                     ,
                     {
                         headers: {
