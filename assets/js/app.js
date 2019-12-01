0550590457dcc2f2bb9dbc0e7b1e515a3c6257e0
Bernard Bos <bb0057329@windesheim.nl>
2019-12-01 16:10:50 +0100

    Don't modify the router

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,24 +6,12 @@ import { store } from './store';
 import AppLogin from './components/AppLogin.vue'
 import CurrentUser from './components/CurrentUser.vue'
 import VueFlashMessage from 'vue-flash-message';
-var VueScrollTo = require('vue-scrollto');
 
-Vue.use(VueScrollTo);
 Vue.use(VueRouter);
 Vue.use(VueFlashMessage);
 
 const router = new VueRouter({
-    routes,
-    scrollBehavior(to, from, savedPosition) {
-        if (to.hash) {
-            VueScrollTo.scrollTo(to.hash);
-            return { selector: to.hash }
-        } else if (savedPosition) {
-            return savedPosition;
-        } else {
-            return { x: 0, y: 0 }
-        }
-    }
+    routes
 });
 
 new Vue({
