14c995fe66a4df97c43abaab0dad0c183c6423de
Bernard Bos <bb0057329@windesheim.nl>
2019-12-02 22:02:43 +0100

    Add basic layout for settings

--- assets/js/components/PageComponents/Settings.vue
+++ assets/js/components/PageComponents/Settings.vue
@@ -1,5 +1,10 @@
 <template>
-    <p>Instellingen</p>
+    <div class="container dashboard-container">
+        <div class="path"><router-link to="/" class="ml-2">Dashboard</router-link> &gt; Instellingen</div>
+        <div class="mt-5 mb-5">
+            <h1>Instellingen</h1>
+        </div>
+    </div>
 </template>
 
 <script>
