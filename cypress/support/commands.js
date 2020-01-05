cfbdcfebde2dd388d8ecac68c9eaf34d26f760d6
Joost Reijmer <s1120128@student.windesheim.nl>
2020-01-05 13:53:52 +0100

    - bug opgelost in appLogin.vue (spinner deed het niet) - finish-pagina getest - basisurl in cypress.json gezet - in cy.route overal gehele api-pad vanaf api erin gezet (om verwarring te voorkomen) - als je dit test: in cypress.json de baseurl vervangen door zoiets als "http://localhost:52093/#/" --> anders zie je niet dat bug opgelost is

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,12 +3,16 @@ Cypress.Commands.add('login', (username, password) => {
   cy.server({
     status: 200
   })
-  cy.route('POST', 'Authenticate', 'fixture:mockAuthenticate').as('login')
-  cy.visit('https://vidden.karenbrakband.nl')
+  cy.route('POST', 'api/User/Authenticate/', 'fixture:mockAuthenticate').as('login')
+  cy.route('GET', 'api/userpreference/getshowgradinginwords/6', false).as('getShowGradingInWords')
+  cy.route('GET', 'api/userpreference/getlanguagepreference/6', 'nl_NL').as('getLanguagePreference')
+  cy.visit('')
   cy.get('input[name=emailaddress]').type(username)
   cy.get('input[name=password]').type(password)
   cy.get('form').submit()
   cy.wait('@login')
+  cy.wait('@getShowGradingInWords')
+  cy.wait('@getLanguagePreference')
 })
 
 Cypress.Commands.add('loginFalse', (username, password) => {
@@ -16,8 +20,8 @@ Cypress.Commands.add('loginFalse', (username, password) => {
   cy.server({
     status: 400,
   })
-  cy.route('POST', 'Authenticate', '').as('loginFalse')
-  cy.visit('https://vidden.karenbrakband.nl')
+  cy.route('POST', 'api/User/Authenticate/', '').as('loginFalse')
+  cy.visit('')
   cy.get('input[name=emailaddress]').type(username)
   cy.get('input[name=password]').type(password)
   cy.get('form').submit()
@@ -28,45 +32,44 @@ Cypress.Commands.add('logout', () => {
   cy.server({
     status: 200
   })
-  cy.route('GET', 'Logout', '').as('logout')
+  cy.route('GET', 'api/User/Logout/', '').as('logout')
   cy.get('div').contains('Uitloggen').click()
   cy.wait('@logout')  
 })
 
 Cypress.Commands.add('openBrowsePage', () => {
   cy.server()
-  cy.route('GET', 'GetAssessments', 'fixture:mockAssessments').as('assessments')
+  cy.route('GET', 'api/Assessment/GetAssessments/', 'fixture:mockAssessments').as('assessments')
   cy.get('a').contains('Bladeren').click()
   cy.wait('@assessments')
 })
 
 Cypress.Commands.add('backToBrowsePage', () => {
   cy.server()
-  cy.route('GET', 'GetAssessments', 'fixture:mockAssessments').as('getAssessments')
+  cy.route('GET', 'api/Assessment/GetAssessments/', 'fixture:mockAssessments').as('getAssessments')
   cy.get('a').contains('Bladeren').click()
   cy.wait(['@getAssessments'])
 })
 
 Cypress.Commands.add('openNewAssessmentPage', () => {
   cy.server()
-  cy.route('GET', 'GetStudents', 'fixture:mockStudents').as('getStudents')
-  cy.route('GET', 'Template', 'fixture:mockTemplate').as('getTemplate')
-  cy.route('GET', 'GetTeachers', 'fixture:mockTeachers').as('getTeachers')
+  cy.route('GET', 'api/Student/GetStudents', 'fixture:mockStudents').as('getStudents')
+  cy.route('GET', 'api/Template/', 'fixture:mockTemplate').as('getTemplate')
+  cy.route('GET', 'api/Teacher/GetTeachers', 'fixture:mockTeachers').as('getTeachers')
   cy.get('a').contains('Nieuw').click()
   cy.wait(['@getStudents', '@getTemplate', '@getTeachers'])
 })
 
 Cypress.Commands.add('openEditAssessmentPage', () => {
   cy.server()
-  cy.route('GET', '6', 'fixture:mockAnswers').as('getAssessmentTeacher')
-  cy.route('GET', 'assessment/1/question/6', 'fixture:mockAnswers').as('getAssessmentTeacher')
-  cy.route('GET', '/api/assessment/GetSummary/1/1/6', 'fixture:mockCompetence1').as('competence1')
-  cy.route('GET', '/api/assessment/GetSummary/1/2/6', 'fixture:mockCompetence2').as('competence2')
-  cy.route('GET', '/api/assessment/GetSummary/1/3/6', 'fixture:mockCompetence3').as('competence3')
-  cy.route('GET', '/api/assessment/GetSummary/1/4/6', 'fixture:mockCompetence4').as('competence4')
-  cy.route('GET', '/api/assessment/GetSummary/1/5/6', 'fixture:mockCompetence5').as('competence5')
-  cy.route('GET', '/api/assessment/GetSummary/1/6/6', 'fixture:mockCompetence6').as('competence6')
-  cy.visit('https://vidden.karenbrakband.nl/#/edit/1/6')
+  cy.route('GET', 'api/assessment/1/question/6', 'fixture:mockAnswers').as('getAssessmentTeacher')
+  cy.route('GET', 'api/assessment/GetSummary/1/1/6', 'fixture:mockCompetence1').as('competence1')
+  cy.route('GET', 'api/assessment/GetSummary/1/2/6', 'fixture:mockCompetence2').as('competence2')
+  cy.route('GET', 'api/assessment/GetSummary/1/3/6', 'fixture:mockCompetence3').as('competence3')
+  cy.route('GET', 'api/assessment/GetSummary/1/4/6', 'fixture:mockCompetence4').as('competence4')
+  cy.route('GET', 'api/assessment/GetSummary/1/5/6', 'fixture:mockCompetence5').as('competence5')
+  cy.route('GET', 'api/assessment/GetSummary/1/6/6', 'fixture:mockCompetence6').as('competence6')
+  cy.visit('edit/1/6')
   cy.wait(['@getAssessmentTeacher'])
   cy.wait(['@competence1'])
   cy.wait(['@competence2'])
@@ -78,7 +81,7 @@ Cypress.Commands.add('openEditAssessmentPage', () => {
 
 Cypress.Commands.add('chooseAnswer', (answer) => {
   cy.server()
-  cy.route('POST', 'AnswerSave', '').as('answerSaved')
+  cy.route('POST', 'api/assessment/AnswerSave', '').as('answerSaved')
   const answerWithHash = '#' + answer;
   cy.get('.assessment__question.d-sm-block').contains('De student heeft een gestructeerde beheeraanpak')
     .parent().get(answerWithHash).click()
@@ -87,7 +90,7 @@ Cypress.Commands.add('chooseAnswer', (answer) => {
 
 Cypress.Commands.add('addComment', (comment, field) => {
   cy.server()
-  cy.route('POST', 'CommentsSave', '').as('commentSaved')
+  cy.route('POST', 'api/assessment/CommentsSave', '').as('commentSaved')
   cy.get('label').contains(field).parent().get('#opmerkingen').type(comment)
   cy.get('h4').contains('Opmerkingen').click()
   cy.wait(['@commentSaved'])
@@ -98,8 +101,8 @@ Cypress.Commands.add('addComment', (comment, field) => {
 
 Cypress.Commands.add('confirmNewAssessment', () => {
   cy.server()
-  cy.route('POST', 'assessment', 13).as('postNewAssessment')
-  cy.route('GET', '13', 'fixture:mockAssessment').as('getNewAssessment')
+  cy.route('POST', 'api/assessment', 13).as('postNewAssessment')
+  cy.route('GET', 'api/Assessment/13', 'fixture:mockAssessment').as('getNewAssessment')
   cy.get('a').contains('Bevestigen').click()
   cy.wait(['@postNewAssessment'])
   cy.wait(['@getNewAssessment'])
@@ -107,20 +110,38 @@ Cypress.Commands.add('confirmNewAssessment', () => {
 
 Cypress.Commands.add('openSummaryPage', () => {
   cy.server()
-  cy.route('GET', '13', 'fixture:mockSummary').as('getSummary')
-  cy.visit('https://vidden.karenbrakband.nl/#/summary/13')
+  cy.route('GET', 'api/Assessment/13', 'fixture:mockSummary').as('getSummary')
+  cy.visit('summary/13')
   cy.wait(['@getSummary'])
 })
 
 Cypress.Commands.add('backToSummaryPage', () => {
   cy.server()
-  cy.route('GET', '1', 'fixture:mockSummary').as('getSummary')
+  cy.route('GET', 'api/Assessment/1', 'fixture:mockSummary').as('getSummary')
   cy.get('a').contains('Samenvatting beoordeling').click()
   cy.wait(['@getSummary'])
 })
 
 Cypress.Commands.add('backToDashboardPage', () => {
   cy.get('a').contains('Dashboard').click()
+})
+
+Cypress.Commands.add('openFinishPage', () => {
+  cy.server()
+  cy.route('GET', 'api/assessment/1/fullsummary/6', 'fixture:mockFullSummary').as('getFullSummary')
+  cy.route('GET', 'api/assessment/1', 'fixture:mockAssessment').as('getAssessment')
+  cy.get('.sidebar').get('.open-button').click()
+  cy.get('span.child-title').contains('Communicatie').click()
+  cy.get('a').contains('Bevestigen').click()
+  //cy.wait(['@getFullSummary'])  
+  cy.wait(['@getAssessment'])  
+})
+
+Cypress.Commands.add('backToEditAssessmentPage', () => {
+  cy.server()
+  cy.route('GET', 'api/assessment/1/question/6', 'fixture:mockAnswers').as('getAssessmentTeacher')
+  cy.get('a').contains('Bewerken').click()
+  cy.wait(['@getAssessmentTeacher'])
 })
 
 Cypress.Commands.add('clearCache', () => {
